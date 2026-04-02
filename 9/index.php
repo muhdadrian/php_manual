51ea838bda99f616ffc95e5615c10c6f756ba552
Adrian Nandu <adriannandu@gmail.com>
2026-04-02 23:42:30 +0800

    ‘2_April_2026’

---
+++ 9/index.php
@@ -0,0 +1,12 @@
+<?php
+# NULL
+
+// The null type is PHP's unit type, i.e. it has only one value: null.
+// Undefined, and unset() variables will resolve to the value null.
+
+# Syntax
+
+// There is only one value of type null, and that is the case-insensitive constant null.
+
+$var = null;
+?>
